f40dcd9f41166acbb496551138dcfa7319950217
Brian Jackson <axnjaxn@axnjaxn.com>
2014-08-07 15:22:41 -0400

    Fixed a huge bug with compression.

--- compressor.cpp
+++ compressor.cpp
@@ -88,68 +88,6 @@ void Compressor::encode(FILE* in, FILE* out) {
 	  entry.index = next;
 	  entry.bit = bit;
 	  scan->zero = new BTree(next++);
-	}
-      }
-      else {
-	//If the pattern's been seen before, continue	if (scan->zero) scan = scan->zero;
-	if (scan->one) scan = scan->one;
-	//Otherwise, prepare the next row and record the pattern
-	else {
-	  entry.index = next++;
-	  entry.bit = bit;
-	  scan->one = new BTree(next++);
-	}
-      }  
-    }
-    
-    //Write this row
-    fwrite(&entry.row, 2, 1, out);
-  }
-
-  delete tree;
-}
-
-void Compressor::decode(FILE* in, FILE* out) {
-  BitWriter writer(out);
-
-#if 0
-  
-  TableRow entry;
-  BTree* tree = new BTree(), *scan;
-  tree->zero = new BTree(0);
-  tree->one = new BTree(1);
-  unsigned int next = 2;
-  bool bit;
-
-  unsigned short magic = Compressor::magic;
-  fwrite(&magic, 2, 1, out);
-  while (!reader.eof()) {
-    //Initialize pattern search
-    scan = tree;
-
-    for (;;) {
-      //If the pattern has no more bits,
-      if (reader.eof()) {
-	//Write a special row
-	entry.row = ~0;
-	fwrite(&entry.row, 2, 1, out);
-
-	//Then break and write the currently matched pattern
-	entry.index = scan->index;
-	entry.bit = 0;
-	break;
-      }
-      
-      //If there are more bits
-      bit = reader.next();
-      if (!bit) {
-	//If the pattern's been seen before, continue
-	if (scan->zero) scan = scan->zero;
-	//Otherwise, prepare the next row and record the pattern
-	else {
-	  entry.index = next;
-	  entry.bit = bit;
-	  scan->zero = new BTree(next++);
 	  break;
 	}
       }
@@ -171,6 +109,6 @@ void Compressor::decode(FILE* in, FILE* out) {
   }
 
   delete tree;
-
-#endif
 }
+
+void Compressor::decode(FILE* in, FILE* out) { }
